3c848cee20b594e2b0ae35827a992a95bd420bed
Yuresh <yureshmee@gmail.com>
2025-02-07 13:38:39 +0545

    removed a

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -15,17 +15,7 @@ export function Welcome() {
             </p>
             <ul>
               {resources.map(({ href, text, icon }) => (
-                <li key={href}>
-                  <a
-                    className="group flex items-center gap-3 self-stretch p-3 leading-normal text-blue-700 hover:underline dark:text-blue-500"
-                    href={href}
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    {icon}
-                    {text}
-                  </a>
-                </li>
+                <li key={href}></li>
               ))}
             </ul>
           </nav>
