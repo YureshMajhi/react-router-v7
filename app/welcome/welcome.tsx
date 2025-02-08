e1f65d401ab1ab922bc35cca2600d8b3e867f456
Yuresh <yureshmee@gmail.com>
2025-02-08 22:00:26 +0545

    test direct commit

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -4,7 +4,7 @@ export function Welcome() {
       <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
         <header className="flex flex-col items-center gap-9"></header>
         <div className="max-w-[300px] w-full space-y-6 px-4"></div>
-        Welcome
+        Welcome To branch changes
       </div>
     </main>
   );
